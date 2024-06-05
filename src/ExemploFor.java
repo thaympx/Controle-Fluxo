06f26252caf520443a99d5cdc2aca40912c536de
Thaline <thalinep12@gmail.com>
2024-06-05 00:57:07 -0300

    committrue

---
+++ src/ExemploFor.java
@@ -0,0 +1,12 @@
+public class ExemploFor {
+    public static void main(String[] args)  {
+
+//for: (para) 
+
+        for (int carneirinhos = 1; carneirinhos <= 20;carneirinhos ++ ){
+            System.out.println("contando carneirinhos" + carneirinhos);
+        }
+
+        System.out.println("joãzinho dormiu");
+    }
+}
